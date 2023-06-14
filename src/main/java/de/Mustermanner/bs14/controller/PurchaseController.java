96f68eb46bc45c4286ba11e3482264924216d5df
ARBEYER <arthur.beyer@otto.de>
2023-06-14 10:14:30 +0200

    Updated HTML file

--- src/main/java/de/Mustermanner/bs14/controller/PurchaseController.java
+++ src/main/java/de/Mustermanner/bs14/controller/PurchaseController.java
@@ -12,51 +12,79 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
-
 @Controller
 @RequestMapping("purchase")
 public class PurchaseController {
 
-    private final PurchaseService repository;
+  private final PurchaseService repository;
 
-    @Autowired
-    public PurchaseController(PurchaseService repository) {
-        this.repository = repository;
-    }
+  @Autowired
+  public PurchaseController(PurchaseService repository) {
+    this.repository = repository;
+  }
 
-    @GetMapping("")
-    @ResponseStatus(HttpStatus.OK)
-    public String getPurchase(Model model) {
-        model.addAttribute("purchases",repository.getPurchases());
-        model.addAttribute("newPurchase", new Purchase());
-        return "new-purchase";
-    }
+  @GetMapping("")
+  @ResponseStatus(HttpStatus.OK)
+  public String getPurchase(Model model) {
+    model.addAttribute("purchases", repository.getPurchases());
+    model.addAttribute("newPurchase", new Purchase());
+    return "new-purchase";
+  }
 
-    @PostMapping("/deletePurchase")
-    public String deletePurchase(
-            @RequestParam String id) {
-        repository.deletePurchase(id);
-        return "redirect:/purchase";
-    }
+  @PostMapping("/deletePurchase")
+  public String deletePurchase(@RequestParam String id) {
+    repository.deletePurchase(id);
+    return "redirect:/purchase";
+  }
 
-    @PostMapping("/createPurchase")
-    public String createPurchase(
-            @ModelAttribute Purchase Purchase, BindingResult errors, Model model) {
-        repository.createPurchase(Purchase);
-        return "redirect:/purchase";
-    }
+  @PostMapping("/createPurchase")
+  public String createPurchase(
+      @ModelAttribute Purchase purchase, BindingResult errors, Model model) {
+    purchase.setShareName(purchase.getShareName().toUpperCase().replace(".","").replace(" ","_"));
+    repository.createPurchase(purchase);
+    return "redirect:/purchase";
+  }
 
-    @GetMapping("/getPurchase/buyer/{buyerId}")
-    public String getPurchasesFromBuyer(
-            @PathVariable String buyerId, BindingResult errors, Model model) {
-        List<Purchase> purchases = repository.getPurchases();
-        List<Purchase> purchasesWithBuyerId = new ArrayList<>();
-        for(Purchase p : purchases){
-            if(p.getBuyerId().equals(buyerId)){
-                purchasesWithBuyerId.add(p);
-            }
-        }
-        return "redirect:/purchase";
+  @GetMapping("/buyer")
+  public String getPurchasesFromBuyer(@RequestParam("id") String id, Model model) {
+    List<Purchase> purchases = repository.getPurchases();
+    List<Purchase> purchasesWithBuyerId = new ArrayList<>();
+    System.out.println(id);
+    for (Purchase p : purchases) {
+      if (p.getBuyerId().equals(id)) {
+        purchasesWithBuyerId.add(p);
+      }
     }
+    model.addAttribute("purchases", purchasesWithBuyerId);
+    return "specific-purchase";
+  }
+
+  @GetMapping("/sharename")
+  public String getPurchasesFromShareName(@RequestParam("name") String id, Model model) {
+    List<Purchase> purchases = repository.getPurchases();
+    List<Purchase> purchasesWithShareName = new ArrayList<>();
+    System.out.println(id);
+    for (Purchase p : purchases) {
+      if (p.getShareName().equals(id)) {
+        purchasesWithShareName.add(p);
+      }
+    }
+    model.addAttribute("purchases", purchasesWithShareName);
+    return "specific-purchase";
+  }
+
+  @GetMapping("/transaction")
+  public String getPurchasesFromTransactionsId(@RequestParam("id") String id, Model model) {
+    List<Purchase> purchases = repository.getPurchases();
+    List<Purchase> purchasesWithTransactionsId = new ArrayList<>();
+    System.out.println(id);
+    for (Purchase p : purchases) {
+      if (p.getTransactionId().equals(id)) {
+        purchasesWithTransactionsId.add(p);
+      }
+    }
+    model.addAttribute("purchases", purchasesWithTransactionsId);
+    return "specific-purchase";
+  }
 
 }
